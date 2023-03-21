8b32feb4dcfed7ebe1c1567b0a2f5652700b2f8c
Ana Vitória Louro Navili <va014863@gmail.com>
2023-03-21 13:43:23 -0300

    :construction: feat: Criação do arquivo JS e variáveis

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// variáveis
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
+
+class Calculator {
+
+}
+
+buttons.forEach((btn) => {
+    
+
+})
