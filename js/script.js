31e46cefb32dc9267dc5bd29844556ced34f902a
Ana Vitória Louro Navili <va014863@gmail.com>
2023-03-21 14:22:49 -0300

    :construction: feat: Construção de evento para os botões

--- js/script.js
+++ js/script.js
@@ -3,11 +3,19 @@ const previousOperationText = document.querySelector("#previous-operation");
 const currentOperationText = document.querySelector("#current-operation");
 const buttons = document.querySelectorAll("#buttons-container button");
 
-class Calculator {
+class Calculator {}
 
-}
-
+// criando o evento para os botões
 buttons.forEach((btn) => {
-    
+    btn.addEventListener("click", (e) => {
+        // selecionando o texto nos botões
+        const value = e.target.innerText;
 
-})
+        // separando botões de números e operações
+        if(+value >= 0 || value === ".") {
+            console.log(value); //numeros
+        } else {
+            console.log("Op: " + value); //operações
+        }
+    });
+});
